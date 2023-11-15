f3dc82f50ff3d40201062ebec2d86460ab7cc2d5
Crowdbotics <team@crowdbotics.com>
2023-11-15 14:07:35 +0000

    Initial commit

---
+++ backend/defense_ventures_44561/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for defense_ventures_44561 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "defense_ventures_44561.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "defense-ventures-44561",
+  url: "https://defense-ventures-44561.botics.co",
+  api: "https://defense-ventures-44561.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
